688edd42011d75d9282f920c1af9c16f860da6ce
PizzaNCoding88 <umbertonardiello@gmail.com>
2025-05-14 09:19:19 +0100

    setting up mobile sidebar

---
+++ src/components/MobileSideBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MobileSideBar = () => {
+  return (
+    <div className="w-full h-full bg-white">
+      <div></div>
+    </div>
+  );
+};
+
+export default MobileSideBar;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,22 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import Logo from "../../public/images/monstercat-logo.webp";
 import Link from "next/link";
+import { AlignJustify } from "lucide-react";
+import MobileSideBar from "./MobileSideBar";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(0);
   return (
     <nav className="h-16 bg-[rgb(24,24,21)] px-8 py-2 flex flex-row items-center justify-between">
       <Image alt="web logo" src={Logo} width={35} height={35} />
-      <Link href="">
+      <Link href="" onClick={() => setIsOpen(!isOpen)}>
         <div className="flex flex-col gap-[6px]">
-          <span className="w-7 h-[3px] bg-[rgba(241,241,241,0.5)] block rounded-full"></span>
-          <span className="w-7 h-[3px] bg-[rgb(241,241,241,0.5)] block rounded-full"></span>
-          <span className="w-7 h-[3px] bg-[rgb(241,241,241,0.5)] block rounded-full"></span>
+          <AlignJustify />
         </div>
       </Link>
+      {isOpen && <MobileSideBar />}
     </nav>
   );
 };
