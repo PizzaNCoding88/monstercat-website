79725f4405489b7f7d9a8884372e1736f13279be
PizzaNCoding88 <umbertonardiello@gmail.com>
2025-05-15 08:37:14 +0100

    music menu

--- src/components/Sidebar/SidebarMenu.js
+++ src/components/Sidebar/SidebarMenu.js
@@ -1,13 +1,31 @@
 import React from "react";
-import { ChevronRight } from "lucide-react";
+import { ChevronRight, ChevronUp } from "lucide-react";
+import { useState } from "react";
 
 const SidebarMenu = () => {
+  const [musicMenu, setMusicMenu] = useState();
+  function openCloseMusicMenu() {
+    setMusicMenu(!musicMenu);
+  }
   return (
     <div>
       <ul className="uppercase flex flex-col gap-4 font-bold text-xl pl-8 pt-1 font-kanit tracking-wide">
-        <div className="flex items-center gap-4 cursor-pointer">
-          <li>Music</li>
-          <ChevronRight size={15} />
+        <div className="flex flex-col">
+          <div
+            className="flex items-center gap-4 cursor-pointer"
+            onClick={openCloseMusicMenu}
+          >
+            <li>Music</li>
+            {musicMenu ? <ChevronUp /> : <ChevronRight />}
+          </div>
+          {musicMenu && (
+            <ul className="pl-6 pt-1 font-light flex flex-col gap-1 text-lg tracking-normal">
+              <li>our music</li>
+              <li>Instinct</li>
+              <li>Uncaged</li>
+              <li>Silk</li>
+            </ul>
+          )}
         </div>
         <li>Artists</li>
         <div className="flex items-center gap-4">
