e1b781ac8a53dd98324e687004ff7db14b2092c1
PizzaNCoding88 <umbertonardiello@gmail.com>
2025-05-21 17:14:47 +0100

    trakclist completed

--- src/components/Tracklist.js
+++ src/components/Tracklist.js
@@ -1,9 +1,55 @@
 import React from "react";
+import Song from "./songs/Song";
 
 const Tracklist = () => {
   return (
-    <div className="pl-5">
-      <h1 className="font-main text-xl uppercase font-bold">Tracklist</h1>
+    <div className="px-5 pb-50">
+      <h1 className="font-main text-xl uppercase font-bold pb-5">Tracklist</h1>
+      <div className="flex flex-col gap-8">
+        <Song
+          index={"1"}
+          title={"The Small Things"}
+          artist={"Conro"}
+          duration={"3:35"}
+        />
+        <Song
+          index={"2"}
+          title={"Without Your Love"}
+          artist={"Conro"}
+          duration={"3:09"}
+        />
+        <Song
+          index={"3"}
+          title={"Therapy"}
+          artist={"Conro"}
+          duration={"3:06"}
+        />
+        <Song
+          index={"4"}
+          title={"Fighters"}
+          artist={"Conro"}
+          duration={"3:46"}
+        />
+        <Song
+          index={"5"}
+          title={"Way Up"}
+          artist={"Conro & Nevve"}
+          duration={"3:32"}
+        />
+        <Song
+          index={"6"}
+          title={"Waiting"}
+          artist={"Conro"}
+          duration={"3:07"}
+        />
+        <Song
+          index={"7"}
+          title={"Dreaming"}
+          artist={"Conro"}
+          duration={"2:57"}
+        />
+        <Song index={"8"} title={"Tattoo"} artist={"Conro"} duration={"3:29"} />
+      </div>
     </div>
   );
 };
---
+++ src/components/songs/Song.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Play, Share2 } from "lucide-react";
+
+const Song = (props) => {
+  const { index, title, artist, duration } = props;
+  return (
+    <div className="flex items-center justify-between">
+      <p className="flex-[10%]">{index}</p>
+      <Play className="flex-[15%] pr-4" />
+      <div className="flex-[65%] font-main pr-10">
+        <p>{title}</p>
+        <p className="text-sm">{artist}</p>
+      </div>
+      <div className="flex gap-2 flex-[10%]">
+        <p>{duration}</p>
+        <Share2 />
+      </div>
+    </div>
+  );
+};
+
+export default Song;
