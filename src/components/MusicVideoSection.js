e7fb721d99b9aaa9dcfdfbbb5b6d18ab43c75294
PizzaNCoding88 <umbertonardiello@gmail.com>
2025-05-21 17:25:28 +0100

    video embedded

--- src/components/MusicVideoSection.js
+++ src/components/MusicVideoSection.js
@@ -2,8 +2,17 @@ import React from "react";
 
 const MusicVideoSection = () => {
   return (
-    <div className="px-5">
+    <div className="px-5 py-10 flex flex-col gap-8">
       <h2 className="uppercase font-main font-bold text-xl">Music Video</h2>
+      <iframe
+        className="w-full aspect-video"
+        src="https://www.youtube.com/embed/7nObtWENgxA?si=UsZnH5ERyEq4naIG"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+      ></iframe>
     </div>
   );
 };
